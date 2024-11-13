ba3d5b69305690ac7ee8703d222598cc53205200
MatheusChagas7 <matheusch7@gmail.com>
2024-11-13 19:33:08 -0300

    Adicionando paginação

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,32 @@
 'use client';
 import { useState } from "react";
 import estilos from "./page.module.css";
+import Image from "next/image";
+import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
+
 
 import Categorias from "./componentes/Categorias/page";
 import CampoDeBusca from "./componentes/CampoDeBusca/page";
 import Card from "./componentes/Card/page";
 
+
 import { retornaCardapio } from "./servico";
 
 export default function Home() {
 
-  const [pratos, setPratos] = useState(retornaCardapio)
+  const [pratos, setPratos] = useState(retornaCardapio);
+  const [paginaAtual, setpaginaAtual] = useState(1);
+  const pratosPorPaginas = 10;
+  const totalDePaginas = Math.ceil(pratos.length / pratosPorPaginas);
+
+  const indiceInicio = (paginaAtual - 1) * pratosPorPaginas;
+  const indiceFim = (indiceInicio + pratosPorPaginas);
+  const pratosExibidos = pratos.slice(indiceInicio, indiceFim);
+
+
+  const mudaPagina = (pagina) => {
+    setpaginaAtual(pagina);
+  }
 
   return (
     <div className={estilos.pagina}>
@@ -27,13 +43,13 @@ export default function Home() {
       </header>
       <main className={estilos.main}>
 
-        <Categorias setPratos={setPratos}/>
+        <Categorias setPratos={setPratos} />
 
-        <CampoDeBusca setPratos={setPratos}/>
+        <CampoDeBusca setPratos={setPratos} />
         <h2>Cardápio</h2>
         <div className={estilos.container_cardapio}>
-          {pratos.map((prato) =>
-            <Card 
+          {pratosExibidos.map((prato) =>
+            <Card
               key={prato.id}
               nome={prato.nome}
               categoria={prato.categoria}
@@ -42,6 +58,24 @@ export default function Home() {
               imagem={prato.imagem}
             />
           )}
+        </div>
+        {/* Controles de Paginação */}
+        <div className={estilos.paginacao}>
+
+          <button
+            onClick={() => mudaPagina(paginaAtual - 1)}
+            disabled={paginaAtual === 1}>
+            <FaArrowCircleLeft />
+          </button>
+
+          <span>{`Página ${paginaAtual} de ${totalDePaginas}`}</span>
+
+          <button
+            onClick={() => mudaPagina(paginaAtual + 1)}
+            disabled={paginaAtual === totalDePaginas}>
+            <FaArrowCircleRight />
+          </button>
+
         </div>
       </main>
     </div>
